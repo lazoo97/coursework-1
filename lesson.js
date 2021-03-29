4bddb8718ce5615a3ebf5731e522653ee84c0c73
Abdulazeez Hashim <hashimabdulazeez97@gmail.com>
2021-03-29 15:03:27 +0400

    add lessons to lesson.js

---
+++ lesson.js
@@ -0,0 +1,110 @@
+let lessons = [
+    {
+        "id": 101,
+        "subject": 'Accounting',
+        "location": "Dubai",
+        "price": 2000,
+        "image": "online-class.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 102,
+        "subject": 'Chemistry',
+        "location": "Ras Al-Khaima",
+        "price": 3500,
+        "image": "online-class.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 103,
+        "subject": 'Biology',
+        "location": "Al Qouz",
+        "price": 10000,
+        "image": "online-class.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 104,
+        "subject": 'Government',
+        "location": "Hatta",
+        "price": 2000,
+        "image": "online-class.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 105,
+        "subject": 'English',
+        "location": "Dubai",
+        "price": 3500,
+        "image": "online-class.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 106,
+        "subject": 'Law',
+        "location": "Al Ain",
+        "price": 10000,
+        "image": "online-class.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 107,
+        "subject": 'History',
+        "location": "Sharjah",
+        "price": 2000,
+        "image": "online-class.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 108,
+        "subject": 'Science',
+        "location": "Ajman",
+        "price": 3500,
+        "image": "online-class.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 109,
+        "subject": 'Math',
+        "location": "Fujairah",
+        "price": 10000,
+        "image": "online-class.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 110,
+        "subject": 'Mechanics',
+        "location": "Ras Al-Khaimah",
+        "price": 2000,
+        "image": "online-class.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 111,
+        "subject": 'Auto repair',
+        "location": "Dubai",
+        "price": 3500,
+        "image": "online-class.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 112,
+        "subject": 'Civil Engineering',
+        "location": "Abu Dhabi",
+        "price": 10000,
+        "image": "online-class.jpg",
+        "availableInventory":5,
+        "rating": 5
+    }
+]
